0d227b74e4eefd166acb17272accff33d94e0f39
Gabriel Sussumu Kato <gabrielsussumu@outlook.com>
2018-06-12 19:27:35 -0300

    Serving index.html instead of a random string

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const path = require('path');
+
+const indexHtml = (() => {
+  return fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf-8');
+})();
 
 app.get('*', (req, res) => {
-  res.write('aaaaaaaa');
+  res.write(indexHtml);
   res.end();
 })
 
