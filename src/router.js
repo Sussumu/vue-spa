f965485c2b2cc0b6c3b86a3a47f278b6ffdb45b6
Gabriel Sussumu Kato <gabrielsussumu@outlook.com>
2018-07-24 19:39:30 -0300

    Login page, changed mode to history

--- src/router.js
+++ src/router.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Category from './theme/Category.vue'
+import Login from './theme/Login.vue'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  routes: [{
-    path: '/', component: Category
-  }]
+  routes: [
+    { path: '/login', component: Login },
+    { path: '/', component: Category }]
 })
 
 export default router
