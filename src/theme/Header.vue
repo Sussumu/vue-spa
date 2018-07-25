2f8ca114a768c5401d0cc3b0ff8c8214437091fc
Gabriel Sussumu Kato <gabrielsussumu@outlook.com>
2018-07-25 19:30:07 -0300

    Parameterized routes

--- src/theme/Header.vue
+++ src/theme/Header.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="tabs">
     <ul>
-      <li><div><router-link to="/category/front-end">Front-End</router-link></div></li>
-      <li><div><router-link to="/category/mobile">Mobile</router-link></div></li>
+      <li><div><router-link :to="{ name: 'category', params: { id: 'front-end' } }">Front-End</router-link></div></li>
+      <li><div><router-link :to="{ name: 'category', params: { id: 'mobile' } }">Mobile</router-link></div></li>
       <li><div><router-link to="/login">Login</router-link></div></li>
     </ul>
   </div>
