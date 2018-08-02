48fa080bff4b80b327ab326d19524c8b4167fe5b
Gabriel Sussumu Kato <gabrielsussumu@outlook.com>
2018-08-02 17:14:09 -0300

    SSR

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,7 +1,10 @@
 const webpack = require('webpack')
 const clientConfig = require('./webpack.base.config')
+const serverConfig = require('./webpack.server.config')
+const MFS = require('memory-fs')
+const path = require('path')
 
-module.exports = function setupDevServer (app) {
+module.exports = function setupDevServer (app, onUpdate) {
   clientConfig.entry.app = [
     'webpack-hot-middleware/client',
     clientConfig.entry.app
@@ -13,12 +16,22 @@ module.exports = function setupDevServer (app) {
 
   const clientCompiler = webpack(clientConfig)
 
-  app.use(require('webpack-dev-middleware')(clientCompiler, {
-    stats: {
-      colors: true
-    }
-  })
+  app.use(
+    require('webpack-dev-middleware')(clientCompiler, {
+      stats: {
+        colors: true
+      }
+    })
   )
 
   app.use(require('webpack-hot-middleware')(clientCompiler))
+
+  // creates server-side files in memory
+  const serverCompiler = webpack(serverConfig)
+  const mfs = new MFS()
+  const outputPath = path.join(serverConfig.output.path, 'server/main.js')
+  serverCompiler.outputFileSystem = mfs
+  serverCompiler.watch({}, () => {
+    ontimeupdate(mfs.readFileSync(outputPath, 'utf-8'))
+  })
 }
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const fs = require('fs')
 const path = require('path')
+const { createBundleRenderer } = require('vue-server-renderer')
+
+let renderer
 
 const indexHtml = (() => {
   return fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf-8')
@@ -9,9 +12,16 @@ const indexHtml = (() => {
 
 app.use('/dist', express.static(path.resolve(__dirname, './dist')))
 
-require('./build/dev-server')(app)
+require('./build/dev-server')(app, bundle => {
+  renderer = createBundleRenderer(bundle)
+})
 
 app.get('*', (req, res) => {
+  renderer.renderToString({ url: req.url }, (err, html) => {
+    if (err)
+      return res.status(500).send('Server Error')
+  })
+  html = indexHtml.replace('{{ APP }}', html)
   res.write(indexHtml)
   res.end()
 })
