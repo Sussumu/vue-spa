e1eafe6759c4e7cd6fee6a6e4f0e39e1d01f031a
Gabriel Sussumu Kato <gabrielsussumu@outlook.com>
2018-08-08 19:21:42 -0300

    Optimizations to build scripts

--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -5,12 +5,12 @@ const config = Object.assign({}, base, {
   plugins: base.plugins || []
 })
 
-config.module.rules
-  .filter(x => { return x.loader === 'vue-loader' })
-  .forEach(x => x.options.extractCSS = true)
+// config.module.rules
+//   .filter(x => { return x.loader === 'vue-loader' })
+//   .forEach(x => x.options.extractCSS = true)
 
-config.plugins.push(
-  new ExtractTextPlugin('assets/styles.css')
-)
+// config.plugins.push(
+//   new ExtractTextPlugin('assets/styles.css')
+// )
 
 module.exports = config
